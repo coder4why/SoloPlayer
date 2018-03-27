022bb1c8d5b1c7509489f4b3d0120fcc30ccfd43
Hpk <13456864167@163.com>
2018-03-27 09:43:46 +0800

    Initial Commit

---
+++ SoloPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoloPlayer
+//
+//  Created by qwer on 2018/3/27.
+//  Copyright © 2018年 qwer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
